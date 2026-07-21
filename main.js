f5f084313ae6a3ca64a81445db1190d1908bce6b
Name SurName <illot1004@gmail.com>
2026-07-22 00:47:24 +0200

    fix: some change

--- main.js
+++ main.js
@@ -3,19 +3,18 @@ import { BackgroundManager } from "./background/BackgroundManager.js";
 import { Engine } from "./core/engine.js";
 import { CharactersManager } from "./character/CharactersManager.js";
 import { DialogueManager } from "./dialogue/DialogueManager.js";
+import { CharactersApi } from "./api/CharactersApi.js";
 
 export function createEngine(options = {}) {
 
     const config = new Config(options);
-    const api = config.charactersApi || new CharactersApi(config.apiBaseUrl);
+    const api = config.charactersApi || new CharactersApi(config.apiBaseUrl || "");
 
     const engine = new Engine(config);
-    
 
     engine.characters = new CharactersManager(api);
     engine.background = new BackgroundManager(config);
     engine.dialogue = new DialogueManager(api);
-
 
     return engine;
 }
